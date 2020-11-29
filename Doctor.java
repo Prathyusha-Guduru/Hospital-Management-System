b5e25e3879df11e0bfd32e918ff8d5855b51bde6
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2020-11-29 19:37:14 +0530

    Updated doctor class

--- Doctor.java
+++ Doctor.java
@@ -1,6 +1,6 @@
-// import java.io.*;
+
 import java.util.*;
-// import java.util.Calendar;
+
 import java.util.HashMap;
 
 
@@ -11,17 +11,18 @@ public class Doctor
     public void new_doctor()
     {
         Scanner input = new Scanner(System.in);
-        System.out.print("id:-");
+        System.out.println("Plese enter the following details : ");
+        System.out.print("ID number  :\t");
         d_id = input.nextLine();
-        System.out.print("name:-");
+        System.out.print("Name :\t");
         d_name = input.nextLine();
-        System.out.print("specilization:-");
+        System.out.print("Specilization :\t");
         specialization = input.nextLine();
-        System.out.print("work time:-");
+        System.out.print("Consultation time :\t");
         appointment = input.nextLine();
-        System.out.print("qualification:-");
+        System.out.print("Qualification :\t");
         qualification = input.nextLine();
-        System.out.print("room no.:-");
+        System.out.print("Cabin number :\t");
         d_room = input.nextLine();
         input.close();
 
@@ -33,7 +34,11 @@ public class Doctor
     {
         // System.out.println(info.values().toString());
         // System.out.println(Arrays.toString(info.entrySet().toArray()));
-        System.out.println(Arrays.toString(info.entrySet().toArray()));
+        System.out.println(info.get(this.d_name));
+        // System.out.print(info.get(this.d_name)[i]);
+
+        System.out.println(d_name + "  :  "+ info.get(this.d_name)[0] + "\t" + info.get(this.d_name)[1] + "  \t" + info.get(this.d_name)[2] + "     \t" + info.get(this.d_name)[3] + "    \t" + info.get(this.d_name)[4]);
+    
     }
 
     public static void main(String[] args) 
